0e25ca878934073adc15e05e094b46efd4a6ca53
MuktadirHassan <tamimmuktadir@gmail.com>
2020-10-06 04:37:43 +0600

    added firebase project id to env and cert

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ module.exports = function (req, res, next) {
         credential: admin.credential.cert({
             "private_key": process.env.FIREBASE_PRIVATE_KEY,
             "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+            "project_id": process.env.FIREBASE_PROJECT_ID
         }),
         database: process.env.FIRE_DB
     })};
--- routes/registeredEvents.js
+++ routes/registeredEvents.js
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
         credential: admin.credential.cert({
             "private_key": process.env.FIREBASE_PRIVATE_KEY,
             "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+            "project_id":process.env.FIREBASE_PROJECT_ID
         }),
         database: process.env.FIRE_DB
     })};
